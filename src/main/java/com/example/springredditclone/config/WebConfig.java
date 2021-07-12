a747eb084f60a65ba38d9e2600b114d9f7aa5cd7
hienthan <hienthan@cleverhub.vn>
2021-07-12 22:59:54 +0700

    Added the WebConfig to fix the CORS issue.

---
+++ src/main/java/com/example/springredditclone/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.example.springredditclone.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*")
+                .maxAge(360L).allowedHeaders("*").allowCredentials(false);
+    }
+
+    /*we get a 404 Error because we added EnableWebMvc and Spring MVC doesn’t know how to handle the Web Jars which are
+     loaded as part of Swagger.*/
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
